b2bab6286207b4f1165d22cc2adc9ae7bc0fac24
Yash Karande <kyashkarande@gmail.com>
2020-05-24 03:33:38 +0530

    exam_result: Write webservice and chaincode to insert result data.

--- chaincode/exam_result/javascript/lib/exam_result.js
+++ chaincode/exam_result/javascript/lib/exam_result.js
@@ -24,6 +24,7 @@ class ExamResult extends Contract {
     }
 
     async queryResult( ctx, emailId ) {
+
         const resultAsBytes = await ctx.stub.getState( emailId );
         if ( !resultAsBytes || resultAsBytes.length === 0 ) {
             throw new Error( `${emailId} does not exist` );
@@ -32,53 +33,17 @@ class ExamResult extends Contract {
         return resultAsBytes.toString();
     }
 
-    // async createResult( ctx, emailId, questionsData, passingCutOff ) {
+    async createResult( ctx, emailId, examQuestions, correctAnswersCount, percentage, grade, questionsCount ) {
 
-    //     let questionsCount = questionsData.length;
-    //     let correctAnswersCount = 0;
-    //     let promise = new Promise( ( resolve, reject ) => {
-	//     for( let i = 0; i < questionsCount; i++ ) {
-	// 	delete questionsData[i]['option_3'];
-	// 	delete questionsData[i]['option_1'];
-	// 	delete questionsData[i]['option_2'];
-	// 	delete questionsData[i]['option_4'];
-	// 	delete questionsData[i]['isBookmarked'];
-	// 	delete questionsData[i]['question'];
-
-	// 	if( questionsData[i]['correct_option'] == questionsData[i]['selectedOption'] ) {
-	// 		correctAnswersCount++ ;
-	// 	}
-	// 	if( i == questionsCount-1 ) resolve();
-    //         }
-    //     });
-
-    //     promise.then( () => {
-
-    //         let percentage = Math.round( correctAnswersCount/questionsCount*100 );
-    //         let grade = '';
-
-    //         if( percentage > passingCutOff ) {
-    //           grade = 'Passed';
-    //         } else {
-    //           grade = 'Failed'
-    //         }
-
-	//      questionsData
-    //         const result = {
-    //             examQuestions: JSON.stringify( questionsData ),
-    //             correctAnswers: correctAnswersCount.toString(),
-    //             percentage: percentage.toString(),
-    //             grade: grade,
-    //             questionsCount: questionsCount.toString()
-    //         }
-
-    //         //buffer = Buffer.from(JSON.stringify( result ));
-    //         await ctx.stub.putState(emailId, Buffer.from(JSON.stringify( result )));
-	//     //console.log(JSON.parse(buffer.toString()));
-    //     });
-
-        
-    // }  
+        const result = {
+            examQuestions,
+            correctAnswersCount,
+            percentage,
+            grade,
+            questionsCount
+        }
+        await ctx.stub.putState(emailId, Buffer.from(JSON.stringify( result )));
+    }  
 
 }
 
--- exam_result/javascript/app.js
+++ exam_result/javascript/app.js
@@ -24,6 +24,9 @@ app.use( ( req, res, next ) => {
 } );
 
 app.get( '/getResult', async ( req, res ) => {
+
+    let emailId = req.query['emailId'];
+    console.log( emailId )
     try {
         const ccpPath = path.resolve( __dirname, '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json' );
         const ccp = JSON.parse( fs.readFileSync(ccpPath, 'utf8') );
@@ -44,13 +47,85 @@ app.get( '/getResult', async ( req, res ) => {
 
         const network = await gateway.getNetwork( 'mychannel' );
         const contract = network.getContract( 'exam_result' );
-        const result = await contract.evaluateTransaction( 'queryResult', 'example@gmail.com' );
+        const result = JSON.parse( await contract.evaluateTransaction( 'queryResult', emailId ) );
+        
+        const responseResult = {
+            correctAnswersCount: result['correctAnswersCount'],
+            examQuestions: JSON.parse( result['examQuestions'] ),
+            grade: result['grade'],
+            percentage: result['percentage'],
+            questionsCount: result['questionsCount']
+        }
 
-        res.status(200).json(JSON.parse(result));
+        res.status(200).json(responseResult);
 
     } catch ( error ) {
         console.error( `Failed to evaluate transaction: ${error}` );
         process.exit( 1 );
+    }
+});
+
+app.post( '/sendResult', async ( req, res ) => {
+
+    let emailId = req.body['emailId'];
+    let questionsData = req.body['questionsData'];
+    let passingCutOff = req.body['passingCutOff'];
+
+    let questionsCount = questionsData.length;
+    let correctAnswersCount = 0;
+    for( let i = 0; i < questionsCount; i++ ) {
+
+        if( questionsData[i]['correct_option'] == questionsData[i]['selectedOption'] ) {
+            correctAnswersCount++ ;
+        }
+    }
+
+    let percentage = Math.round( correctAnswersCount/questionsCount*100 );
+    let grade = '';
+    if( percentage > passingCutOff ) {
+        grade = 'Passed';
+    } else {
+        grade = 'Failed'
+    }
+    console.log(JSON.stringify(questionsData));
+    console.log(questionsCount.toString());
+    console.log(percentage.toString());
+    console.log(grade.toString());
+    console.log(correctAnswersCount.toString());
+    try {
+        
+        const ccpPath = path.resolve(__dirname, '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
+        let ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        const identity = await wallet.get('appUser');
+        if (!identity) {
+            console.log('An identity for the user "appUser" does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            return;
+        }
+
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
+
+        const network = await gateway.getNetwork('mychannel');
+
+        const contract = network.getContract('exam_result');
+        await contract.submitTransaction('createResult', emailId, JSON.stringify( questionsData ), correctAnswersCount.toString(), percentage.toString(), grade.toString(), questionsCount.toString() );
+        console.log('Transaction has been submitted');
+
+        const response = {
+            result: 'Transaction has been submitted'
+        }
+
+        res.status(200).json(response);
+
+    } catch (error) {
+        console.error(`Failed to submit transaction: ${error}`);
+        process.exit(1);
     }
 });
 
